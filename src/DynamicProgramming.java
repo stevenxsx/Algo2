e3609e67ead4319e0c2df84fa2ef354054534daa
S <stevenxsx@gmail.com>
2022-10-14 13:55:15 +0200

    Working Manhattan Tourists

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -3,26 +3,41 @@ public class DynamicProgramming {
     public static void main(String[] args) { new DynamicProgramming().run(); }
 
     public void run() {
-        int n = 25;
-        int[] memoize = new int[n+1];
+        int n = 50;
+        long[] memoize = new long[n+1];
 
-        long optimizedStart = System.nanoTime();
-        int optimized = fibonacciOptimized(n, memoize);
-        long optimizedStop = System.nanoTime();
+        long optimizedStart;
+        long optimizedStop;
         long exponentialStart;
         long exponentialStop;
-        exponentialStart= System.nanoTime();
-        int exponential = fibonacciExponential(n);
+        long bottomUpStart;
+        long bottomUpStop;
+
+        //Measuring runtime of functions.
+        optimizedStart = System.nanoTime();
+        long optimized = fibonacciOptimized(n, memoize);
+        optimizedStop = System.nanoTime();
+
+        exponentialStart = System.nanoTime();
+        //long exponential = fibonacciExponential(n);
         exponentialStop = System.nanoTime();
+
+        bottomUpStart = System.nanoTime();
+        long bottomUp = fibonacciBottomUp(n);
+        bottomUpStop = System.nanoTime();
+
         System.out.println("Fibonacci Optimized result -> " + optimized);
         System.out.println("Optimized solution took " + (optimizedStop-optimizedStart) + " nanoseconds.");
-        System.out.println("Fibonacci Exponential result -> " + exponential);
+        //System.out.println("Fibonacci Exponential result -> " + exponential);
         System.out.println("Exponential solution took " + (exponentialStop-exponentialStart) + " nanoseconds.");
+        System.out.println("Fibonacci BottomUp result -> " + bottomUp);
+        System.out.println("BottomUp solution took " + (bottomUpStop-bottomUpStart) + " nanoseconds.");
         System.out.println("Exponential solution took " + ((exponentialStop-exponentialStart) / (optimizedStop-optimizedStart)) + " times the optimized runtime.");
+        System.out.println("Exponential solution took " + ((exponentialStop-exponentialStart) / (bottomUpStop-bottomUpStart)) + " times the bottomUp runtime.");
 
     }
 
-    public int fibonacciOptimized(int n, int[] memoize) {
+    public long fibonacciOptimized(int n, long[] memoize) {
         if (memoize[n] != 0) { //If the value has already been calculated before.
             return memoize[n];
         }
@@ -30,18 +45,30 @@ public class DynamicProgramming {
             return 1;
         }
         else {
-            int result = fibonacciOptimized(n-1,memoize) + fibonacciOptimized(n-2,memoize); //Recursion using memoize
+            long result = fibonacciOptimized(n-1,memoize) + fibonacciOptimized(n-2,memoize); //Recursion using memoize
             memoize[n] = result;
             return result;
         }
     }
 
-    public int fibonacciExponential(int n) {
+    public long fibonacciExponential(int n) {
         if (n == 1 || n == 2) { //Fibonacci sequence starts with 1, 1
             return 1;
         }
         else {
             return fibonacciExponential(n-1) + fibonacciExponential(n-2);
         }
+    }
+
+    public long fibonacciBottomUp(int n) {
+        int one = 1;
+        int two = 1;
+        for (int i = 3; i < n; i++) {
+            if (i%2 == 1) {
+                two = one+two;
+            }
+            else { one = one+two; }
+        }
+        return one+two;
     }
 }
--- src/ManhattanTourists.java
+++ src/ManhattanTourists.java
@@ -1,33 +1,72 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ManhattanTourists {
 
+
+
     int size;
-    public int[] s;
-    public int[] b;
+    public int[][] r;
+    public int[][] d;
+    public int[][] m;
 
     public void run() {
+
         Scanner sc = new Scanner(System.in);
-        size = sc.nextInt();
-        s = new int[size];
-        b = new int[size];
+        size = sc.nextInt(); // Size = 1 - 1000
+        r = new int[size][size-1];
+        d = new int[size-1][size];
+        m = new int[size][size];
+        for (int[] row : m) //Initialize entire multidimensional array to -1
+            Arrays.fill(row,-1);
+
         for (int i = 0; i < size; i++) {
-            s[i] = sc.nextInt();
+            for (int j = 0; j < size-1; j++) {
+                r[i][j] = sc.nextInt();
+            }
         }
-        for (int i = 0; i < size; i++) {
-            b[i] = sc.nextInt();
+        for (int i = 0; i < size-1; i++) {
+            for (int j = 0; j < size; j++) {
+                d[i][j] = sc.nextInt();
+            }
         }
 
-        System.out.println(tourism());
+        //System.out.println(Arrays.deepToString(r));
+        //System.out.println(Arrays.deepToString(d));
+        System.out.println(tourism(size-1,size-1));
+        //System.out.println(Arrays.deepToString(m));
 
     }
 
-    public int tourism() {
+    public int tourism(int i, int j) {
+        if (m[i][j] != -1) {
+            return m[i][j]; //Utilize memoization.
+        }
 
+        if (i == 0 && j == 0) {
+            m[i][j] = 0;
+        }
+        else if (i > 0 && j == 0) {
+            m[i][j] = tourism(i-1,j)+d[i-1][j];
+        }
+        else if (i == 0 && j > 0) {
+            m[i][j] = tourism(i,j-1)+r[i][j-1];
+        }
+        else {
+            m[i][j] = (Math.max(tourism(i-1,j)+d[i-1][j],tourism(i,j-1)+r[i][j-1]));
+        }
+        return m[i][j];
     }
 
+    /**
+     * Recurrence: opt (i,j) =  { 0              -> if i = 0 and j = 0
+     *                          { vi + opt(i-1,0)-> if i > 0 and j = 0
+     *                          { vj + opt(0,j-1)-> if j > 0 and i = 0
+     *                          { max of { vi + opt(i-1,j)
+     *                                   { vj + opt(i,j-1)
+     */
 
     public static void main(String[] args) {
-        new ChocolateAgency().run();
+        new ManhattanTourists().run();
     }
 }
