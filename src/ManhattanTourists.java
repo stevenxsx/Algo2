2a08bf1e8222431f6c50ad1a4a4dcb15280d3d1a
S <stevenxsx@gmail.com>
2022-10-14 12:50:36 +0200

    Initial code

--- src/ManhattanTourists.java
+++ src/ManhattanTourists.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class ManhattanTourists {
+
+    int size;
+    public int[] s;
+    public int[] b;
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        size = sc.nextInt();
+        s = new int[size];
+        b = new int[size];
+        for (int i = 0; i < size; i++) {
+            s[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        System.out.println(tourism());
+
+    }
+
+    public int tourism() {
+
+    }
+
+
+    public static void main(String[] args) {
+        new ChocolateAgency().run();
+    }
 }
