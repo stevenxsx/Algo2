78fac87084ea5322d858c800b8d3a551fec5bf6c
S <stevenxsx@gmail.com>
2022-10-24 13:00:55 +0200

    Bug with the output part.

--- src/SequenceAlignment.java
+++ src/SequenceAlignment.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SequenceAlignment {
@@ -9,8 +10,11 @@ public class SequenceAlignment {
     int m;
     int n;
     int[][] memo;
+    direction[][] dir;
     String x;
     String y;
+    String outputX;
+    String outputY;
 
     public void run() {
         Scanner sc = new Scanner(System.in);
@@ -18,22 +22,36 @@ public class SequenceAlignment {
         y = sc.nextLine(); //String Y
         m = x.length(); //Length m of x
         n = y.length(); //Length n of y
-        memo = new int[m+1][n+1];
-        for (int i = 0; i < m; i++) {
-            memo[i][0] = i*d;
+        memo = new int[m + 1][n + 1];
+        dir = new direction[m + 1][n + 1];
+        for (int[] row : memo) { //Initialize entire multidimensional array to -1
+            Arrays.fill(row, -1);
         }
-        for (int j = 0; j < n; j++) {
-            memo[0][j] = j*d;
+        for (int i = 0; i < m; i++) { //Initialize first row with the only possible values being skip costs.
+            memo[i][0] = i * d;
+            dir[i][0] = direction.l;
+        }
+        dir[m][n] = direction.d;
+        for (int j = 0; j < n; j++) { //The same for the first column
+            memo[0][j] = j * d;
+            dir[0][j] = direction.u;
         }
 
-        for (int j = 1; j <= n; j++) {
+        /*for (int j = 1; j <= n; j++) {
             for (int i = 1; i <= m; i++) {
                 opt(i,j);
             }
-        }
-        System.out.println(memo[m][n]);
+        }*/
+        System.out.println(opt(m - 1, n - 1));
+        //System.out.println(Arrays.deepToString(memo));
+        calculateOutput();
+        printDir();
+        System.out.println(outputX);
+        System.out.println(outputY);
 
     }
+
+    public enum direction {l, u, d,o}
 
     public int opt(int i, int j) {
         /**
@@ -54,21 +72,82 @@ public class SequenceAlignment {
          * Similarly, with case (III), we get opt(m,n) = δ + opt(m,n-1)
          */
 
-        if (true /*case 1*/) {
-            memo[i][j] = mismatchCost(x.charAt(i),y.charAt(j)) + opt(m-1,n-1);
-            return memo[i][j];
+        if (memo[i][j] != -1) {
+            return memo[i][j]; //Utilize memoization.
         }
+        //return memo[i][j] = Math.min(Math.min(d + opt(i - 1, j),d + opt(i, j - 1)),matchCost(x.charAt(i), y.charAt(j)) + opt(i - 1, j - 1));
 
+        int one = matchCost(x.charAt(i), y.charAt(j)) + opt(i - 1, j - 1);
+        int two = d + opt(i - 1, j);
+        int three = d + opt(i, j - 1);
 
-
-
-        memo[i][j] = Math.min(Math.min(1,2),3);
-
-    return -1;
+        //Returns the smallest of the 3 possible cases, while also setting the value in the matrix, and direction in the other
+        if (three < two && three < one) {
+            //up
+            dir[i][j] = direction.u;
+            memo[i][j] = three;
+            return three;
+        } else if (two < one && two < three) {
+            //left
+            dir[i][j] = direction.l;
+            memo[i][j] = two;
+            return two;
+        } else {
+            //diagonal
+            dir[i][j] = direction.d;
+            memo[i][j] = one;
+            return one;
+        }
 
     }
 
-    public int mismatchCost(char a, char b) {
+    public void calculateOutput() {
+        outputX = "";
+        outputY = "";
+
+        int i = m;
+        int j = n;
+
+        while (!(i == 0 && j == 0)) { //When we reach (0,0) it terminates.
+            direction d = dir[i][j];
+            dir[i][j] = direction.o;
+            if (d == direction.d) {
+                outputX = x.charAt(i - 1) + outputX;
+                outputY = y.charAt(j - 1) + outputY;
+                i--;
+                j--;
+            } else if (d == direction.u) {
+                outputX = "-" + outputX;
+                outputY = y.charAt(j - 1) + outputY;
+                j--;
+            } else if (d == direction.l) {
+                outputX = x.charAt(i - 1) + outputX;
+                outputY = "-" + outputY;
+                i--;
+            } else {
+                System.out.println("error");
+            }
+            System.out.println(outputX);
+            System.out.println(outputY);
+        }
+
+    }
+
+    public void printDir() {
+        for (int i = 0; i < dir[0].length; i++) {
+            for (int j = 0; j < dir.length; j++) {
+                int k = memo[j][i];
+                if (k < 10) {
+                    System.out.print("[0");
+                }
+                else { System.out.print("["); }
+                System.out.print(memo[j][i] + "]");
+            }
+            System.out.println("");
+        }
+    }
+
+    public int matchCost(char a, char b) {
         if (a == b) {
             return 0;
         }
